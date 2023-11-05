aa6ebdd19ca037eb89cde0a93e8a64fa1be792b0
kittijaaa <78350987+kittijaaa@users.noreply.github.com>
2023-11-05 18:09:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'App.jsx'
+import App from './App.jsx'
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files with the correct MIME type
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'application/javascript');
+    }
+  },
+}));
+
+// Serve your React app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
